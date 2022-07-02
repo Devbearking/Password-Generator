7a1ca92d5a9a4ebc1c92beda9968204ada37c7cd
Deyvid.R <102469283+Devbearking@users.noreply.github.com>
2022-07-02 11:48:26 +0300

    Update passwordgen.py

--- passwordgen.py
+++ passwordgen.py
@@ -7,27 +7,6 @@ alphanum="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 print(alphanum.lower())
 spalphanum="abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ#@+"
 
-# passlen = int (input ("enter password lenght \n"))
-# randPass = []
-
-# print("Select the type of password \nl. Numbers\n2. Alphanum\n3. Special alphanumeric\n")
-# Selecttype = int(input())    
-
-# if Selecttype == 1: 
-#     for i in range(passlen) :
-#         randPass.append (choice (num))
-# elif (Selecttype == 2):
-#     for i in range(passlen) :
-#             randPass.append (choice (alphanum))
-# else:
-#     for i in range(passlen) :
-#         randPass.append (choice (spalphanum))
-
-# result = "".join(randPass)
-
-# print("your random passowrd is : "+result)
-
-
 def Create_Pass():
     TheChoise = Tchoise.get()
 
@@ -50,43 +29,45 @@ def Create_Pass():
 
 
 
-
+def rgb_hack(rgb):
+    return "#%02x%02x%02x" % rgb
 
 
 
 
 window = Tk()
+window.config(bg=rgb_hack((173,216,230)))
 window.geometry("800x500")
 window.title("Random Password Genetaror")
 
-Progname = Label(window,font=('Kristen ITC',15,'bold'),text="Password Genetrator (^_^)",fg="blue")
+Progname = Label(window,font=('Comic Sans MS',15,'bold'),text="Password Genetrator",fg="blue",bg='SkyBlue')
 Progname.grid(row=1,column=3,padx=200,pady=30)
 
-ChooseType = Label (window,font=('Kristen ITC',15,'bold'),text="Choosetype a type among 3")
-ChooseType.place(relx=.03,rely=.25)
+ChooseType = Label (window,font=('Comic Sans MS',15,'bold'),text="Choosetype a type among 3",bg='SkyBlue')
+ChooseType.place(relx=.1,rely=.25)
 
 Tchoise = StringVar()
-NumChoise = Radiobutton(window,font=('cornel',10,'italic'),text ="Numeric", variable = Tchoise,value=num) 
-NumChoise.place(relx=.3, rely = .35)
-AlphaNumChoise = Radiobutton(window,font=('cornel',10,'italic'),text ="AlphaNumeric", variable = Tchoise,value=alphanum) 
-AlphaNumChoise.place(relx=.3, rely = .4)
-SpecialChoise = Radiobutton(window,font=('cornel',10,'italic'),text ="Special", variable = Tchoise,value=spalphanum) 
-SpecialChoise.place(relx=.3, rely = .45)
+NumChoise = Radiobutton(window,font=('cornel',10,'italic'),text ="Numeric", variable = Tchoise,value=num,bg='SkyBlue')
+NumChoise.place(relx=.1, rely = .35)
+AlphaNumChoise = Radiobutton(window,font=('cornel',10,'italic'),text ="AlphaNumeric", variable = Tchoise,value=alphanum,bg='SkyBlue')
+AlphaNumChoise.place(relx=.1, rely = .4)
+SpecialChoise = Radiobutton(window,font=('cornel',10,'italic'),text ="Special", variable = Tchoise,value=spalphanum,bg='SkyBlue')
+SpecialChoise.place(relx=.1, rely = .45)
 
 
-size =Label (window,text="Size",font=('Copperplate Gothic',10,'bold'))
-size.place(relx=.65 , rely =.4 )
+size =Label (window,text="Size",font=('Copperplate Gothic',10,'bold'),bg='SkyBlue')
+size.place(relx=.55 , rely =.4 )
 
-pass_length = Spinbox(window,font_=8,to=50)
-pass_length.place(relx=.7 , rely=.4)
+pass_length = Spinbox(window,font=8,to=50)
+pass_length.place(relx=.6 , rely=.4)
 pass_length.config(state="readonly")
 
 
 
-GenButton = Button(window,text = "Generate",command=Create_Pass)
+GenButton = Button(window,text = "Generate",command=Create_Pass,bg='SkyBlue')
 GenButton.place(relx=.45 , rely=.60)
 
-resultBox = Text(window, height=6, width=85, wrap=WORD)
+resultBox = Text(window, height=6, width=85, wrap=WORD,bg='SkyBlue')
 resultBox.place(relx=.08, rely=.7)
 
 
